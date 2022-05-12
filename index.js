7e83c33ce8c228af568f4a0f29725b726d66b439
MushinShin <facundofrancia3@hotmail.com>
2022-05-12 12:42:38 -0300

    dsadas

---
+++ index.js
@@ -0,0 +1,16 @@
+import { MELI_CLASSES, DB_CLASSES, MELI_URL, DB_URL } from './modules/selectorCONSTS.js';
+import { GET_PRICES, GET_TITLES, GET_URL} from './modules/getTitle-getPriceBDMELI.js';
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch({headless: false, slowMo: 100,});
+  const page = await browser.newPage();
+  // Next line of code is the webpage scrapy will capture data from
+  await page.goto(DB_URL);
+  
+
+  
+
+
+  await browser.close();
+})();
